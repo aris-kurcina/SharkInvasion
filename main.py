a75591a2e7a3fba2a54d0c337e0517ee9e4f636d
aris-kurcina <44555733+aris-kurcina@users.noreply.github.com>
2018-12-21 16:29:29 -0800

    initial  commit

---
+++ main.py
@@ -0,0 +1,14 @@
+import sys
+import pygame
+sys.path.insert(0,"./src")
+from src import game
+
+def init():
+    pygame.init()
+
+
+def run():
+    init()
+
+
+run()
---
+++ src/game.py
@@ -0,0 +1,8 @@
+import sys
+from pygame import *
+import constants
+
+
+class Game:
+    def __init__(self):
+        pass
